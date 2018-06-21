6df47237cf43628e82fe4850e0df7fb47be854a4
darkman13 <parkwooyoung81@gmail.com>
2018-06-21 23:58:03 +0900

    first commit

---
+++ src/main/kotlin/com/darkman13/brandstore/zoocoffee/ZoocoffeeApplication.kt
@@ -0,0 +1,11 @@
+package com.darkman13.brandstore.zoocoffee
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ZoocoffeeApplication
+
+fun main(args: Array<String>) {
+    runApplication<ZoocoffeeApplication>(*args)
+}
---
+++ src/test/kotlin/com/darkman13/brandstore/zoocoffee/ZoocoffeeApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.darkman13.brandstore.zoocoffee
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ZoocoffeeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
